a483d209aff9fdd10a4f55fd221ed6115ccb31f9
kejiehuang <kh3348@columbia.edu>
2025-02-20 11:34:26 -0500

    Update getbondprice_enumerate_kh3348.py

--- getbondprice_enumerate_kh3348.py
+++ getbondprice_enumerate_kh3348.py
@@ -13,11 +13,3 @@ def getBondPrice_E(face,couponRate,m,yc):
   pv_face=face/(1+yc[-1])**m
   bondPrice=pv_coupons+pv_face
   return (bondPrice)
-
-#test Values
-yc=[0.010,0.015,0.020,0.025,0.030]
-face=2000000
-couponRate=0.04
-m=5
-
-getBondPrice_E(face,couponRate,m,yc)
